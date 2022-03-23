d8a01dca4e09cb2d44f7d682cdb9dff6de6cb5ab
Victor Hugo Piguave Perea <victor.piguave@exodosoft.com>
2022-03-23 09:32:39 -0500

    first commit

--- src/connection.js
+++ src/connection.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
-const {  db_host, db_name, db_pass, db_user  } = require('./setting')
+const {  db_host, db_name, db_pass, db_user, db_dialect  } = require('./setting')
 const conn = new Sequelize(db_name, db_user, db_pass,{
     host: db_host,
-    dialect: 'mariadb' || 'mysql'
+    dialect: db_dialect
 })
 
 module.exports = conn
--- src/setting.js
+++ src/setting.js
@@ -7,5 +7,7 @@ module.exports = {
     db_host : process.env.DB_HOST,
     db_user : process.env.DB_USER,
     email : process.env.EMAIL,
-    password : process.env.PASSWORD
+    password : process.env.PASSWORD,
+    db_dialect: process.env.DB_DIALECT,
+    
 }
